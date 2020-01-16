953df43a234b825497ac929b0f54d0e4b1e113f2
Josh <joshhess13@gmail.com>
2020-01-15 23:24:10 -0700

    Added more to contract

--- pages/src/App.vue
+++ pages/src/App.vue
@@ -1,13 +1,23 @@
 <template>
 	<div id="app">
 		<div id="nav">
-			<div class="nav-but" @click="onClick('/')">
+			<div class="nav-button" @click="onClick('/')">
 				<div class="nav-text">Home</div>
 			</div>
-			<div class="nav-but" @click="onClick('/contract')">
-				<div class="nav-text">Contract</div>
+			<div class="nav-dropdown">
+				<div class="nav-button nav-dropdown-button">
+					<div class="nav-text">Contract</div>
+				</div>
+				<div class="nav-dropdown-content">
+					<div
+						class="nav-text nav-dropdown-item"
+						@click="onClick('/contract')"
+					>
+						V1
+					</div>
+				</div>
 			</div>
-			<div class="nav-but" @click="onClick('/about')">
+			<div class="nav-button" @click="onClick('/about')">
 				<div class="nav-text">About</div>
 			</div>
 		</div>
@@ -28,13 +38,14 @@
 <style>
 	* {
 		font-family: 'Montserrat', sans-serif;
+		color: #222222;
 		width: 100%;
 		height: 100%;
 		padding: 0;
 		margin: 0;
 		overflow: hidden;
 	}
-
+	/* --- Navbar --- */
 	#nav {
 		background-color: #1e1e1e;
 		width: 100%;
@@ -43,7 +54,7 @@
 		justify-content: center;
 		flex-direction: row;
 	}
-	.nav-but {
+	.nav-button {
 		text-align: center;
 		width: 120px;
 		display: flex;
@@ -51,13 +62,46 @@
 		justify-content: center;
 		cursor: pointer;
 	}
-	.nav-but:hover {
+
+	.nav-button:hover {
 		background-color: #3d3d3d;
 	}
+
 	.nav-text {
 		height: 20px;
 		color: #cccccc;
 		text-emphasis: none;
 		text-decoration: none;
+		cursor: pointer;
+	}
+
+	/* --- Dropdown --- */
+	.nav-dropdown {
+		width: 120px;
+		float: left;
+		overflow: hidden;
+	}
+	.nav-dropdown-content {
+		width: 120px;
+		height: auto;
+		display: none;
+		position: absolute;
+		background-color: #f9f9f9;
+		box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+		z-index: 1;
+	}
+	.nav-dropdown-item {
+		height: 45px;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.nav-dropdown-item:hover {
+		background-color: #999999;
+	}
+	.nav-dropdown:hover .nav-dropdown-content {
+		display: block;
 	}
 </style>
--- pages/src/views/contracts/Contract.v1.vue
+++ pages/src/views/contracts/Contract.v1.vue
@@ -1,52 +1,115 @@
 <template>
 	<div class="cbackground">
 		<div id="cbody">
-			<h1>Student Project Contract</h1>
-			<h2>Submitted on January 15, 2020</h2>
-			<h2>Submitted by: Josh Hess</h2>
+			<h1>Capstone Project Contract</h1>
+			<h2>{{ getDate() }}</h2>
+			<h2>Josh Hess</h2>
 			<br />
-			<h2>Project Description:</h2>
+
+			<h2>Project Description</h2>
 			<p>
-				(Describe your project in as much detail as you can. Include
-				such things as what questions you are going to research, who
-				this is going to benefit, how the project encompasses what you
-				have learned here at SUU, and any other information that will
-				convince your instructor to approve your project. The more
-				detail the better.)
+				This project is essentially a web server that runs a host of
+				table-top gaming software to assist and enable a seamless
+				creation to play experience. The finer details of each
+				individual component will be found in the objectives section,
+				but a high level review can be found here. To start, the project
+				will primarily entail 3 codebases with a potential variation on
+				2 of them.
+			</p>
+			<li>
+				<b>Backend</b> The actual web server doing all the heavy
+				lifting. In the simpler design schema this will be packaged as
+				an executable and ran locally. Under this scenario this is what
+				would get published to steam and offered as a download. In the
+				more complex schema the web server would be hosted in the cloud
+				and access would be offered via a subscription model. This model
+				would fit in perfectly with the <i>design-as-a-service</i> trend
+				that has become so popular.
+			</li>
+			<li>
+				<b>Frontend</b> The fronted would be doing heavy lifting of it's
+				own but of a differnt nature. This would be the real meat of the
+				application. In either scenario, the frontend would be hosted by
+				the backend. However, in the more complicated scenario a full
+				login/account system would have to be implemented.
+			</li>
+			<li>
+				<b>Website</b> Under either scenario a informational website
+				would be hosted to showcase the product and direct potential
+				customers toward purchasing the product. For the forseeable
+				future this would be hosted with github pages. You may even be
+				reading this contract from there.
+			</li>
+			<p>
+				The core funcitons offered by the software will be as follows:
+				map editor, character creator, campaign manager, and finally the
+				ability to use all the created assets in a gameplay scenario.
 			</p>
 			<br />
-			<h2>Main Objectives:</h2>
-			<p>
-				List here items you are fully committed to getting done. Not
-				getting these items done means you did not fully complete your
-				project.
-			</p>
+
+			<h2>Main Objectives</h2>
+			<li>
+				<b>Map Editor</b> capabilities including creation,
+				customization, saving, and loading. The creation and
+				customization is limited to a single tilemap layer. Nothing
+				further.
+			</li>
+			<li>
+				<b>Campaign management</b> the main part of this is simply to be
+				able to group maps and notes together.
+			</li>
+			<li>
+				<b>Viewing</b> of the map for gameplay. The main part of this
+				being able to switch between a editor and map maker mode.
+			</li>
 			<br />
-			<h2>Secondary Objectives:</h2>
-			<p>
-				List here objectives that you hope to get to, but that you might
-				not complete. You must complete most of these objectives to get
-				full credit.
-			</p>
+
+			<h2>Secondary Objectives</h2>
+			<li>
+				<b>Map Editor</b> the secondary portion aims to offer a layer on
+				top of the tilemap for more detailed items. This include but is
+				not limited to rocks, trees, items, characters, and text.
+			</li>
+			<li>
+				<b>Campaign management</b> here the next step would be the
+				ability to add characters to a campaign to allow stat tracking.
+			</li>
+			<li>
+				<b>Viewing</b> here this would entail multiple viewing modes:
+				viewer, user, or game master. This means there will be three
+				modes of gameplay operation. One will allow simple 'dumb'
+				viewing of whatever map is loaded for the sessions campaign. The
+				second will enable viewing the map, limited interaction with the
+				map, and switching to a stats mode. Finally the third will allow
+				complete control of the map, all stats features of all
+				characters and whater notes are stored with the campaign. This
+				of course necessarily entails a larger objective in that the
+				sessions between different users would have to be synced in some
+				way. (More on this later as the design becomes more clear.)
+			</li>
 			<br />
-			<h2>Tertiary Objectives:</h2>
-			<p>
-				These are your dream objectives. You are doubtful you’ll get to
-				complete any of them, but it would be awesome if you did so.
-				Completing one of these puts you into the above and beyond
-				category. 100% credit would mean you completed all main, all
-				secondary, and at least one tertiary objective
-			</p>
+
+			<h2>Tertiary Objectives</h2>
+			<li>
+				A service as a subsription model. This would include registering
+				as a business with paypal or some payment service. Integration
+				of their api to the website. Having the website integrate to the
+				backend project to allow payed for members to acces content from
+				multiple devices.
+			</li>
+			<li>
+				A forum to allow users to share creations with a wider
+				community.
+			</li>
 			<br />
-			<h2>Submissible Items:</h2>
-			<p>
-				Create a list of items you are going to submit as part of your
-				report. If the only item you are going to submit is the written
-				report, then state that.
-			</p>
-			<br />
-			<br />
-			<br />
+
+			<h2>Submissible Items</h2>
+			<li>
+				All source code for the backend, frontend, and website repos.
+			</li>
+			<li>
+				An executable of the local version of the web server.
+			</li>
 			<br />
 			<p>
 				I will assess the final project based on the goals and
@@ -70,9 +133,7 @@
 			<br />
 		</div>
 		<div class="cprint" @click="onPrint">
-			<div class="ctext">
-				Print
-			</div>
+			<div class="ctext">Print</div>
 		</div>
 	</div>
 </template>
@@ -83,6 +144,26 @@
 		methods: {
 			onPrint: function() {
 				this.$htmlToPaper('cbody');
+			},
+			getDate: function() {
+				let months = [
+					'January',
+					'February',
+					'March',
+					'April',
+					'May',
+					'June',
+					'July',
+					'August',
+					'September',
+					'October',
+					'November',
+					'December'
+				];
+				let d = new Date();
+				return `${
+					months[d.getMonth()]
+				} ${d.getDate()}, ${d.getFullYear()}`;
 			}
 		}
 	};
@@ -91,6 +172,9 @@
 	* {
 		height: auto;
 		width: auto;
+	}
+	li {
+		margin-left: 2em;
 	}
 	.cbackground {
 		background-color: #cccccc;
