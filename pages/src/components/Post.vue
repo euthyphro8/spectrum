c6da1e5fe48f6803cde3cdf2630259ff9714c99a
Josh <joshhess13@gmail.com>
2020-01-21 01:48:00 -0700

    Updated pages homepage to have posts.

---
+++ pages/src/components/Post.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="post-body">
+		<h1>{{ title }}</h1>
+		<h2>{{ date }}</h2>
+		<!-- <img src="@/assets/logo.png" /> -->
+		<img :src="require(`@/assets/posts/${img}`)" />
+		<div class="post-content">
+			<h3>{{ subtitle }}</h3>
+			<p>{{ body }}</p>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'post',
+		methods: {},
+		props: {
+			title: String,
+			date: String,
+			img: String,
+			subtitle: String,
+			body: String
+		}
+	};
+</script>
+<style scoped>
+	* {
+		height: auto;
+		width: auto;
+	}
+	.post-body {
+		min-height: min-content;
+		max-width: 960px;
+		border-radius: 60px;
+		width: calc(92% - 140px);
+		margin: 2% 2%;
+		padding: 45px 70px;
+		background-color: #777777;
+	}
+	h1 {
+		width: 100%;
+		text-align: center;
+	}
+	h2 {
+		width: 100%;
+		text-align: center;
+		margin-bottom: 20px;
+	}
+	h3 {
+		width: 100%;
+	}
+	img {
+		border-radius: 15px;
+		width: 84%;
+		display: block;
+		margin: 0 auto;
+	}
+	p {
+		width: 100%;
+	}
+	.post-content {
+		margin-top: 20px;
+		border-radius: 30px;
+		width: calc(100% - 60px);
+		padding: 30px 30px;
+		background-color: #555555;
+	}
+</style>
--- pages/src/views/Home.vue
+++ pages/src/views/Home.vue
@@ -1,41 +1,66 @@
 <template>
 	<div class="home">
-		<br />
-		<h1>
-			Spectrum
-		</h1>
-		<h2>
-			RPG Campaign Maker
-		</h2>
-		<br />
-		<p>
-			This software aims to...
-		</p>
+		<Post
+			:title="'[ Spectrum ] v0.1.1'"
+			:date="'January 21, 2020'"
+			:img="'basic-tile-draw.gif'"
+			:subtitle="'Tile Palette'"
+			:body="
+				'A very basic tile palette has been added allowing selection and editing the current tilemap. Highlight'
+			"
+		/>
+		<Post
+			:title="'[ Spectrum ] v0.1.0'"
+			:date="'January 16, 2020'"
+			:img="'basic-move-pan.gif'"
+			:subtitle="'Panning/Scrolling'"
+			:body="
+				'The first thing to get out of the way was rendering of the actual sprites on some tilemap. Next a simple pan/scroll was implemented using WASD, Arrow keys, middle-click-n-drag, shift-click-n-drag and the scroll wheel.'
+			"
+		/>
+		<Post
+			:title="'[ Spectrum ] Initial Commit'"
+			:date="'January 14, 2020'"
+			:img="'setup.png'"
+			:subtitle="'Setup'"
+			:body="
+				'First things first, created a git repo with the three planned projects. Next it didn\'t take long to git the \'Pages\' project building and being deployed to a Github Pages site.'
+			"
+		/>
+		<Post
+			:title="'[ Spectrum ] Project Start'"
+			:date="'January 9, 2020'"
+			:img="'logo.png'"
+			:subtitle="'Summary'"
+			:body="
+				'Spectrum aims to be a host of table-top gaming software to assist and enable a seamless ' +
+					'creation to play experience. The finer details of each ' +
+					'individual component will be found in the objectives section,' +
+					'but a high level review can be found here. To start, the ' +
+					'project will primarily entail 3 codebases with a potential ' +
+					'variation on 2 of them.'
+			"
+		/>
 	</div>
 </template>
 
 <script>
+	import Post from '../components/Post.vue';
 	export default {
 		name: 'home',
-		components: {}
+		components: {
+			Post
+		}
 	};
 </script>
 <style scoped>
-	h1 {
-		height: auto;
-		text-align: center;
-	}
-	h2 {
-		height: auto;
-		text-align: center;
-	}
-	p {
-		height: auto;
-		text-align: center;
-	}
 	.home {
 		background-color: #cccccc;
-		overflow-y: auto;
 		height: calc(100% - 60px);
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		overflow-y: auto;
 	}
 </style>
