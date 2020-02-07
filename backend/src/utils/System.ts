c29e1c71dfdcb8b4866eb638f1e655a8e145ac3b
Josh <joshhess13@gmail.com>
2020-02-07 00:55:36 -0700

    tweaked spawn child function

--- backend/src/utils/System.ts
+++ backend/src/utils/System.ts
@@ -2,7 +2,8 @@ import { spawn } from 'child_process';
 
 export default class System {
 	public static RestartUpdate(): void {
-		spawn('npm run update', {
+		spawn('npm', {
+			argv0: 'run update',
 			detached: true,
 			stdio: ['ignore', process.stdout, process.stderr]
 		});
